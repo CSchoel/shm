62fcce8672fc8bc54fcfbea4c6cd675d6bb6c44d
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-04-06 17:58:34 +0200

    implements rmssd

--- tests/python/test.py
+++ tests/python/test.py
@@ -232,7 +232,7 @@ class TestSHMModel(unittest.TestCase):
 		#sdann = 0     
 
 		# root mean squared successive differences (rmssd)
-		rmssd = 0
+		rmssd = rmsd(hr[1:]-hr[:-1])
 
 		# proportion of number of successive interval differences greater than 50 ms (pnn50)
 		# - not recommended by task force of ESC and NASPE => not implemented
