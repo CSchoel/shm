ebf57dd653e67819aa1dd46a1b192f7c5e7b2999
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-05-24 15:28:48 +0200

    adds load methods for more databases

--- tests/python/extract_hrv.py
+++ tests/python/extract_hrv.py
@@ -39,6 +39,36 @@ def load_fantasia(dname):
 	old = [(name(x),ra(x)) for x in glob.glob(os.path.join(dname, "*o*_ann.txt"))]
 	return {"healthy_young": young, "healthy_old": old }
 
+def load_ptbdb(dname):
+	reason_prefix = "# Reason for admission:"
+	data = {}
+	for h in glob.glob(os.path.join(dname, "patient*/*.hea")):
+		name = "ptb_" + os.path.basename(os.path.dirname(h)) + "_" + os.path.basename(h)[:-4]
+		cls = None
+		with open(h, "r", encoding="utf-8") as hf:
+			lines = hf.readlines()
+		for l in lines:
+			if l.startswith(reason_prefix):
+				cls = l[len(reason_prefix):].strip().lower()
+		if cls is None:
+			print("could not find class for record %s" % h)
+		else:
+			if cls.startswith("healthy"):
+				# use string "healthy" instead of "Healthy control"
+				cls = "healthy"
+			data[cls] = data.get(cls,[])
+			data[cls].append((name,ra(h[:-4]+"_ann.txt")))
+	return data
+
+def load_nsr(dname, prefix="nsr"):
+	name = lambda x: prefix + "_" + os.path.basename(x)[:-8]
+	data = [(name(x), ra(x)) for x in glob.glob(os.path.join(dname, "*_ann.txt"))]
+	return {"healthy" : data}
+
+def load_prcp(dname):
+	name = lambda x: "prcp_" + os.path.basename(x)[:-13]
+	data = [(name(x), ra(x)) for x in glob.glob(os.path.join(dname, "*_ann_wqrs.txt"))]
+	return {"healthy_moving" : data}	
 
 if __name__ == '__main__':
 	fantasia = "D:/Daten/physionet/fantasia" 
