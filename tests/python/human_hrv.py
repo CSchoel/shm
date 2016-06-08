842f5977668159d780176ffbe95b00f361e662b0
Christopher Schoelzel <christopher.schoelzel@mni.thm.de>
2016-06-08 22:48:54 +0200

    adds qq and histogram plots

--- tests/python/human_hrv.py
+++ tests/python/human_hrv.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 import os
 import itertools as it
 import glob
+from scipy.stats import norm
+from scipy.stats.mstats import mquantiles
 
 def load_db(dbdir, names=None, combine=False):
 	data = {}
@@ -38,6 +40,46 @@ def plot_poincare(fname, data):
 	fig.savefig(fname+".png")
 	plt.close(fig)
 
+def plot_qq(fname, data):
+	rr = to_rr(data)
+	pc = poincare(rr)
+	# project onto sd1 diagonal
+	ax1 = np.array([-1,1])
+	sd1 = np.dot(pc,ax1) / np.linalg.norm(ax1)
+	sd1 = (sd1 - np.mean(sd1)) / np.std(sd1)
+	lim = (-4,4)
+	nbins = 50
+	xvals = np.arange(nbins,dtype="float32")/nbins
+	q_data = mquantiles(sd1, xvals)
+	q_gauss = [norm.ppf(x) for x in xvals]
+	fig = plt.figure(figsize=(8,8))
+	ax = fig.add_subplot(111)
+	#xvals = bins[:-1] 
+	#bin_width = bins[1]-bins[0]
+	#ax.bar(xvals, np.log(h), bin_width, label="actual")
+	ax.plot(q_data,q_gauss,"bo")
+	ax.set_xlim(lim)
+	ax.set_ylim(lim)
+	fig.savefig(fname+"_qq.png")
+	plt.close(fig)
+	
+
+def plot_hist(fname, data):
+	rr = to_rr(data)
+	pc = poincare(rr)
+	# project onto sd1 diagonal
+	ax1 = np.array([-1,1])
+	sd1 = np.dot(pc,ax1) / np.linalg.norm(ax1)
+	lim = (-1000, 1000)
+	h,bins = np.histogram(sd1, 30, lim)
+	fig = plt.figure(figsize=(8,8))
+	ax = fig.add_subplot(111)
+	xvals = bins[:-1] 
+	bin_width = bins[1]-bins[0]
+	ax.bar(xvals, np.log(h), bin_width, label="actual")
+	fig.savefig(fname+"_hist.png")
+	plt.close(fig)
+
 def is_flat(data):
 	return isinstance(next(iter(data.values())), np.ndarray)
 
@@ -55,6 +97,8 @@ def make_plots(db, dname):
 				print("ERROR: too few datapoints!")
 			else:
 				plot_poincare(os.path.join(kdir,n), ar)
+				plot_hist(os.path.join(kdir,n), ar)
+				plot_qq(os.path.join(kdir,n), ar)
 
 if __name__ == "__main__":
 	dbdir = "D:/Daten/hrvdb"
