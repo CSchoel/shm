cf54aaa9818beb27045120623e22932c7c8f76f5
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2019-05-15 14:19:13 +0200

    updates compare script to better show location of PVCs

--- scripts/plot_mc_compare.py
+++ scripts/plot_mc_compare.py
@@ -21,8 +21,13 @@ def plot_mc(time, modular, orig, outname=None):
     plt.savefig(outname)
   plt.close()
 
-def plot_pvc(time_s, modular_s, time_nos, modular_nos, outname=None):
+def plot_pvc(time_s, modular_s, pvc_s, time_nos, modular_nos, pvc_nos, outname=None):
+  x_pvc_s = time_s.values[pvc_s]
+  x_pvc_nos = time_nos.values[pvc_nos]
+  pvc_labels = ["a)", "b)", "c)", "d)"]
   f, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(10, 4))
+  for x in x_pvc_s:
+    ax1.axvline(x, label="PVC", color="black", linestyle="--", alpha=0.5)
   ax1.plot(time_s, modular_s)
   ax1.set_ylabel("RR interval length [s]")
   ax1.set_xlabel("time[s]")
@@ -30,18 +35,20 @@ def plot_pvc(time_s, modular_s, time_nos, modular_nos, outname=None):
   ax1.set_title("With SA node")
   ax1.minorticks_on()
   #ax1.grid(alpha=0.5, which="both")
-  for (x, y, s) in [(6, 0.95, "a)"), (11, 0.85, "b)"), (14.5, 0.85, "c)"), (20, 1.00, "d)")]:
-    ax1.annotate(s, (x, y))
+  for (x, s) in zip(x_pvc_s, pvc_labels):
+    ax1.annotate(s, xy=(x, 1), xytext=(4, 0), textcoords="offset points")
+  for x in x_pvc_nos:
+    ax2.axvline(x, label="PVC", color="black", linestyle="--", alpha=0.5)
   ax2.plot(time_nos, modular_nos)
   ax2.set_ylabel("RR interval length [s]")
   ax2.set_xlabel("time[s]")
-  ax2.set_xlim(0, 50)
+  ax2.set_xlim(0, 55)
   ax2.set_ylim(0.5, 2)
   ax2.set_title("Without SA node")
   ax2.minorticks_on()
   #ax2.grid(alpha=0.5, which="both")
-  for (x, y, s) in [(12, 1.9, "a)"), (23, 1.9, "b)"), (32, 1.9, "c)"), (42, 1.9, "d)")]:
-    ax2.annotate(s, (x, y))
+  for (x, s) in zip(x_pvc_nos, pvc_labels):
+    ax2.annotate(s, xy=(x, 1.93), xytext=(4, 0), textcoords="offset points")
   plot_scenarios(ax3)
   f.tight_layout()
   if outname is None:
@@ -65,17 +72,19 @@ def plot_scenarios(ax=None):
   l = ax.plot([0.8, 0.8 + t_refrac_avn], [h_signal + dist*2, h_signal + dist*2], label="AV refractory period", **kwargs)
   c = l[0].get_color()
   ax.plot([1.2, 1.2 + t_refrac_avn], [h_signal + dist*1, h_signal + dist*1], "--", color=c, **kwargs) # , label="AV ref. per. (after PVC)"
-  ax.plot([1.5, 1.5 + t_refrac_avn], [h_signal + dist*2, h_signal + dist*2], "--", color=c, **kwargs)
+  ax.plot([1.6 - t_delay/2, 1.6 - t_delay/2 + t_refrac_avn], [h_signal + dist*2, h_signal + dist*2], "--", color=c, **kwargs)
   ax.plot([0.8 + t_delay, 0.8 + t_delay + t_refrac_vent], [h_signal + dist*3, h_signal + dist*3], label="ventr. refractory period", **kwargs)
   ax.set_xlim(0.7, 2)
   ax.set_ylim(0, h_signal + dist*10)
   ax.set_yticks([])
-  ax.set_xticks([0.8 + t_delay/2, 0.8 + t_refrac_avn/2, 1.2, 1.5])
+  ax.set_xticks([0.8 + t_delay/2, 0.8 + t_refrac_avn/2, 1.2, 1.6 - t_delay/2])
   ax.set_xticklabels(["a)", "b)", "c)", "d)"])
   ax.set_xlabel("time")
   ax.legend(loc="best")
+  ax.set_title("PVC locations in SA/AV cycle")
 
-
+def find_pvcs(trigger):
+  return np.where(np.array(trigger[:-1]) < np.array(trigger[1:]))
 
 
 if __name__ == "__main__":
@@ -107,7 +116,9 @@ if __name__ == "__main__":
   plot_pvc(
     pvc_sinus["time"],
     pvc_sinus["T"],
+    find_pvcs(pvc_sinus["trigger"]),
     pvc_nosinus["time"],
     pvc_nosinus["T"],
+    find_pvcs(pvc_nosinus["trigger"]),
     outname=os.path.join(result_folder, "plot_pvc.pdf")
   )
