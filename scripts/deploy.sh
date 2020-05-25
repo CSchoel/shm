44e51478790917eaba9c541acf7e037db1522527
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-05-25 22:17:20 +0200

    updates deploy script to not remove whole deploy folder but only subfolders

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 deploy=~/Documents/Promotion/code/modelica-deploy
-if [ -d "$deploy" ]; then
-  echo Removing $deploy ...
-  rm -rf "$deploy"
+if [ -d "$deploy/SHM" ]; then
+  echo Removing $deploy/SHM ...
+  rm -rf "$deploy/SHM"
+fi
+if [ -d "$deploy/SHMConduction" ]; then
+  echo Removing $deploy/SHMConduction ...
+  rm -rf "$deploy/SHMConduction"
 fi
 echo Copying files from ../SHM to $deploy
-mkdir "$deploy"
 cp -R ../SHM "$deploy/"
 cp -R ../subprojects/shm-conduction/SHMConduction "$deploy/"
