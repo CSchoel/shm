e0c1dc92c3f2ac20b1efae5c1c67e5ddaeeb30a9
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-10-20 16:25:03 +0200

    adds and uses reference data in subproject

--- scripts/unittests.jl
+++ scripts/unittests.jl
@@ -1,15 +1,18 @@
 using ModelicaScriptingTools
 using Test
 
-refdir = "refData"
+refdir = "subprojects/shm-ref"
+# only simulate full models for two seconds in order to reduce execution time
+# of tests and size of reference data files
+twosec = Dict("stopTime" => 2, "numberOfIntervals" => 2000)
 
 withOMC("test-output", ".") do omc
     @testset "Simulations" begin
         @testset "Kotani2005" begin
-            testmodel(omc, "SHM.Kotani2005.Examples.FullModel.KotaniFullExample"; refdir=refdir, override=Dict("stopTime" => "10"))
+            testmodel(omc, "SHM.Kotani2005.Examples.FullModel.KotaniFullExample"; refdir=refdir, override=twosec)
         end
         @testset "SeidelThesis" begin
-            testmodel(omc, "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample"; refdir=refdir)
+            testmodel(omc, "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample"; refdir=refdir, override=twosec)
         end
         @testset "SchoelzelThesis" begin
             testmodel(omc, "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample"; refdir=refdir)
