22824d67799a402f191e6c7f765386f921748ad9
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-10-20 15:09:35 +0200

    puts tests in combined testset to run them every time

--- scripts/unittests.jl
+++ scripts/unittests.jl
@@ -1,21 +1,23 @@
 using ModelicaScriptingTools
 using Test
 
-refdir = ""
+refdir = "refData"
 
 withOMC("test-output", ".") do omc
-    @testset "Kotani2005" begin
-        testmodel(omc, "SHM.Kotani2005.Examples.FullModel.KotaniFullExample"; refdir=refdir, override=Dict("stopTime" => "10"))
-    end
-    @testset "SeidelThesis" begin
-        testmodel(omc, "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample"; refdir=refdir)
-    end
-    @testset "SchoelzelThesis" begin
-        testmodel(omc, "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample"; refdir=refdir)
-        testmodel(omc, "SHM.SchoelzelThesis.Examples.BidirectionalContractionExample"; refdir=refdir)
-        testmodel(omc, "SHM.SchoelzelThesis.Examples.BidirectionalContractionMixinExample"; refdir=refdir)
-    end
-    @testset "shm-conduction" begin
-        testmodel(omc, "SHMConduction.Examples.ModularExample"; refdir=refdir)
+    @testset "Simulations" begin
+        @testset "Kotani2005" begin
+            testmodel(omc, "SHM.Kotani2005.Examples.FullModel.KotaniFullExample"; refdir=refdir, override=Dict("stopTime" => "10"))
+        end
+        @testset "SeidelThesis" begin
+            testmodel(omc, "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample"; refdir=refdir)
+        end
+        @testset "SchoelzelThesis" begin
+            testmodel(omc, "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample"; refdir=refdir)
+            testmodel(omc, "SHM.SchoelzelThesis.Examples.BidirectionalContractionExample"; refdir=refdir)
+            testmodel(omc, "SHM.SchoelzelThesis.Examples.BidirectionalContractionMixinExample"; refdir=refdir)
+        end
+        @testset "shm-conduction" begin
+            testmodel(omc, "SHMConduction.Examples.ModularExample"; refdir=refdir)
+        end
     end
 end
