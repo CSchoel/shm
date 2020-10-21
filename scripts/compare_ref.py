cdb427f5680f8090a261f7cafceef3ac67425133
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2020-10-21 10:59:12 +0200

    makes script more generic

--- scripts/compare_ref.py
+++ scripts/compare_ref.py
@@ -4,8 +4,11 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    ref = pandas.read_csv("subprojects/shm-ref/SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv")
-    cur = pandas.read_csv("test-output/SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv")
-    plt.plot(ref["time"], ref["lung.resp.phase"])
-    plt.plot(cur["time"], cur["lung.resp.phase"])
+    reffile = "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv"
+    # refvar = "lung.resp.phase"
+    refvar = "blood.vessel.pressure"
+    ref = pandas.read_csv("subprojects/shm-ref/{}".format(reffile))
+    cur = pandas.read_csv("test-output/{}".format(reffile))
+    plt.plot(ref["time"], ref[refvar])
+    plt.plot(cur["time"], cur[refvar])
     plt.show()
