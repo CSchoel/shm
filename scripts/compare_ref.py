e14c428b7f864159447cc843b8d5056668ef9914
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-06-10 18:06:42 +0200

    changes compare script to compare UnidirectionalModularExample

--- scripts/compare_ref.py
+++ scripts/compare_ref.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     reffile = "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv"
+    reffile = "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample_res.csv"
     # refvar = "lung.resp.phase"
     # refvar = "blood.vessel.pressure"
     # refvar = "para.signal.activation"
-    refvar = "heart.contraction.T"
+    # refvar = "heart.contraction.T"
+    refvar = "count_mco"
     ref = pandas.read_csv("subprojects/shm-ref/{}".format(reffile))
     cur = pandas.read_csv("test-output/{}".format(reffile))
     plt.plot(ref["time"], ref[refvar])
