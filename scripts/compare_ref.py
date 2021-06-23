1d4edda70db86477b052a963101a81e832dc84a7
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-06-23 11:55:00 +0200

    changes name of reffile to resfile

--- scripts/compare_ref.py
+++ scripts/compare_ref.py
@@ -5,19 +5,20 @@ import matplotlib.pyplot as plt
 import os
 
 if __name__ == "__main__":
-    reffile = "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv"
-    reffile = "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample_res.csv"
-    reffile = "SHM.SchoelzelThesis.Examples.AVDelayExample_res.csv"
+    resfile = "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv"
+    resfile = "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample_res.csv"
+    resfile = "SHM.SchoelzelThesis.Examples.AVDelayExample_res.csv"
+    reffile = "subprojects/shm-ref/{}".format(resfile)
     # refvar = "lung.resp.phase"
     # refvar = "blood.vessel.pressure"
     # refvar = "para.signal.activation"
     # refvar = "heart.contraction.T"
     # refvars = [refvar]
     refvars = ["countOut", "countOuts"]
-    withref = os.path.exists("subprojects/shm-ref/{}".format(reffile))
+    withref = os.path.exists(reffile)
     if withref:
-        ref = pandas.read_csv("subprojects/shm-ref/{}".format(reffile))
-    cur = pandas.read_csv("test-output/{}".format(reffile))
+        ref = pandas.read_csv(reffile)
+    cur = pandas.read_csv("test-output/{}".format(resfile))
     for i, refvar in enumerate(refvars):
         if withref:
             plt.plot(ref["time"], ref[refvar], color="C%d" % i)
