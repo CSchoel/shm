ff23e78606250c552f217b9895292786639ea865
Christopher Schölzel <christopher.schoelzel@mni.thm.de>
2021-06-23 11:42:55 +0200

    checks AVDelayExample

--- scripts/compare_ref.py
+++ scripts/compare_ref.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 if __name__ == "__main__":
     reffile = "SHM.SeidelThesis.Examples.FullModel.SeidelThesisFullExample_res.csv"
     reffile = "SHM.SchoelzelThesis.Examples.UnidirectionalModularExample_res.csv"
-    #reffile = "SHM.SchoelzelThesis.Examples.MultiDelayExample_res.csv"
+    reffile = "SHM.SchoelzelThesis.Examples.AVDelayExample_res.csv"
     # refvar = "lung.resp.phase"
     # refvar = "blood.vessel.pressure"
     # refvar = "para.signal.activation"
     # refvar = "heart.contraction.T"
-    refvar = "count_mco"
-    ref = pandas.read_csv("subprojects/shm-ref/{}".format(reffile))
+    refvar = "countOut"
+    #ref = pandas.read_csv("subprojects/shm-ref/{}".format(reffile))
     cur = pandas.read_csv("test-output/{}".format(reffile))
-    plt.plot(ref["time"], ref[refvar])
+    #plt.plot(ref["time"], ref[refvar])
     plt.plot(cur["time"], cur[refvar], "--")
     plt.show()
